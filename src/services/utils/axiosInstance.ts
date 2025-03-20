e6f605c68998607b49c56e18c49e296013b04eb1
Aquib349 <aquib_m@optimusbt.net>
2025-03-20 23:55:38 +0530

    code: new update

--- src/services/utils/axiosInstance.ts
+++ src/services/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://imslb-878196465.us-east-1.elb.amazonawas.com",
+  baseURL: "http://imslb-878196465.us-east-1.elb.amazonaws.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
